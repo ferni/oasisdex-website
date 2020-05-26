fe2d0847920c850eec19a4316e8acaca2f686191
pacxiu <nedvedek111@gmail.com>
2020-05-26 09:23:03 +0200

    Add section Quick stats [ch2735]

--- components/QuickStats.js
+++ components/QuickStats.js
@@ -1,15 +1,53 @@
 /** @jsx jsx */
-import { jsx, Box, Flex, Container, Heading } from 'theme-ui';
+import {
+  jsx,
+  Box,
+  Flex,
+  Text,
+  Container,
+  Link,
+  Heading,
+  Divider,
+} from 'theme-ui';
 
 const QuickStats = () => {
   return (
     <Box sx={{ backgroundColor: 'surface', pt: 5, pb: 3 }}>
-      <Container>
-        <Flex>
-          <Heading>Some Data</Heading>
-          <Heading>Some Data</Heading>
-          <Heading>Some Data</Heading>
+      <Container sx={{ fontFamily: 'heading' }}>
+        <Flex sx={{ justifyContent: 'space-between', mb: 5, mt: 3 }}>
+          <Box sx={{ textAlign: 'center' }}>
+            <Heading>$10.21M</Heading>
+            <Text color="onSurfaceAlt">Total Volume</Text>
+          </Box>
+          <Box sx={{ textAlign: 'center' }}>
+            <Heading>$1.21M</Heading>
+            <Text color="onSurfaceAlt">Daily Volume</Text>
+          </Box>
+          <Box sx={{ textAlign: 'center' }}>
+            <Heading>55,000</Heading>
+            <Text color="onSurfaceAlt">Daily trades</Text>
+          </Box>
         </Flex>
+        <Box>
+          <Divider
+            sx={{
+              maxWidth: '170px',
+              textAlign: 'center',
+              mx: 'auto',
+              opacity: '0.7',
+            }}
+          />
+          <Text
+            sx={{
+              color: 'onSurfaceAlt',
+              fontSize: 5,
+              textAlign: 'center',
+              my: 4,
+            }}
+          >
+            More at <Link>communitystats.com</Link>
+          </Text>
+        </Box>
       </Container>
     </Box>
   );
