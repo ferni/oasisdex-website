63c844161057b6c884ce92822a8e6e51fbe27a47
Phil Bain <bain.pmcg@gmail.com>
2020-06-08 17:33:06 +0200

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const slug = require('remark-slug');
-const mdxTableOfContents = require('./tocModule');
 
 const withMDX = require('@next/mdx')({
   options: {
     remarkPlugins: [slug],
-    compilers: [mdxTableOfContents],
   },
 });
 module.exports = withMDX({
