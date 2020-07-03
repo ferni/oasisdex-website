aa07e29af50a82de4451a00cb670790349268de1
ferni <fertiberti@gmail.com>
2020-07-03 20:16:29 +0000

    Deploying to gh-pages from  @ 9d9f926e03e1c11d1b77c794bafa75d0943343bd 🚀

---
+++ _next/static/T4NRbNuohM4KMCDyfpjNE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/docs/introduction":[a],"/docs/use-proxy":[a],"/faq":[a],"/index":[a]}}("static\u002Fchunks\u002Fcb64d3211a4fbf9ea359971e6e3cd1b6dc7817d9.691917fc87d13a0fd9fa.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
